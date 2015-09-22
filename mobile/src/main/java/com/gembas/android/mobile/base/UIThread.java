30f791eb1c4b945eccf32cc97ec3f77e06f83340
César Díez Sánchez <cesaryomismo@gmail.com>
2015-09-22 12:26:22 +0200

    added UIThread to the presentation layer

---
+++ mobile/src/main/java/com/gembas/android/mobile/base/UIThread.java
@@ -0,0 +1,23 @@
+/**
+ * Copyright (C) 2014 android10.org. All rights reserved.
+ *
+ * @author Fernando Cejas (the android10 coder)
+ */
+package com.gembas.android.mobile.base;
+
+import com.gembas.android.domain.base.executor.PostExecutionThread;
+
+import rx.Scheduler;
+import rx.android.schedulers.AndroidSchedulers;
+
+/**
+ * MainThread (UI Thread) implementation based on a {@link rx.Scheduler} which will execute actions
+ * on the Android UI thread
+ */
+public class UIThread implements PostExecutionThread {
+
+    @Override
+    public Scheduler getScheduler() {
+        return AndroidSchedulers.mainThread();
+    }
+}
